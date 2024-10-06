5e23fb493719ba39f3e66c7b63f13d8dad6f4edb
WenHong Huang <wayne123tw@gmail.com>
2024-10-06 16:51:14 +0800

    feat(auth): add auth guard to admin & admin products page

--- app/admin/product/page.tsx
+++ app/admin/product/page.tsx
@@ -1,9 +1,14 @@
-import { Product } from "@/components/dashboard/product/product";
+'use client';
 
-export default function Home() {
+import { Product } from "@/components/dashboard/product/product";
+import withAuthRequired from "@/components/hoc/with-auth-required";
+
+function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-1">
             <Product />
         </main>
     );
 }
+
+export default withAuthRequired(Home);
